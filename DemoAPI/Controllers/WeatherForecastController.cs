d734e410fc244935c140992cdd39241a77435779
DESKTOP-T3PLO6P\manish.parmar <manish.parmar@softwebsolutions.com>
2023-02-10 16:02:37 +0530

    changes regarding buidspec yml

--- DemoAPI/Controllers/WeatherForecastController.cs
+++ DemoAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DemoAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Manish", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
